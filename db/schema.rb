89fd7d65021b3c77318ec9ca88a50bf3b744fcc1
pepper-chicken-dev <pepperchicken1992@gmail.com>
2026-02-19 23:33:36 +0900

    feat: add unique constraint on users provider and provider_uid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_17_134142) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_19_142721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_17_134142) do
     t.string "provider", null: false
     t.string "provider_uid"
     t.datetime "updated_at", null: false
+    t.index ["provider", "provider_uid"], name: "index_users_on_provider_and_provider_uid", unique: true
   end
 
   create_table "wordbooks", force: :cascade do |t|
