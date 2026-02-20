6252de971fe7ca68ebf335be20ab05a1b6f339da
pepper-chicken-dev <pepperchicken1992@gmail.com>
2026-02-20 23:30:27 +0900

    Prevent duplicate registration with same email across different providers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :wordbooks, dependent: :destroy
   has_one :setting, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, uniqueness: { allow_nil: true }
   validates :provider, presence: true
   validates :provider_uid, presence: true, uniqueness: { scope: :provider }
 end
