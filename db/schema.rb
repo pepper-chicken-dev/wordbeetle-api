c35de55f9d5bc386b74fbcfea9bfc17f19507457
pepper-chicken-dev <pepperchicken1992@gmail.com>
2026-02-21 15:25:55 +0900

    Add unique index to users.email at DB level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_21_061439) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_21_062453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_21_061439) do
     t.string "provider", null: false
     t.string "provider_uid", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "provider_uid"], name: "index_users_on_provider_and_provider_uid", unique: true
   end
 
